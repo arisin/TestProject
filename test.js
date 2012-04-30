c1b5d9a42c79fc4d7479cf30c8daafc0235ef60b
arisin <monokesi4120@gmail.com>
2012-04-30 22:46:55 +0900

    4/30

--- test.js
+++ test.js
@@ -1,17 +1,39 @@
 // JavaScript Document
 //このサイトを参考にしました：http://gihyo.jp/dev/serial/01/crossbrowser-javascript/0017
 
-//体
-var begin = new Date() - 0;
 
-var	w = document.getElementById('b');
+window.addEventListener("load", function(){
+	
+	//初期設定の値
+	var top = 300;
+	var left = 500;
+	
+	//体
+	var begin = new Date().getTime();//時間を取得（ミリ秒）
 
-var id = setInterval(function(){
-	var current = new Date() - begin;
-  	if (current > 1000){
-    	clearInterval(id);
-    	current = 1000; // 1000以上になっているので，調整する
-  	}	
-  w.style.top = current / 10 + 'px';
-  w.style.left = current / 10 + 'px';
-}, 10); // 10ms置きに実行
+	var	w = document.getElementById('w');
+
+	var id = setInterval(function(){	//繰り返しタイマーイベント
+		var current = new Date() - begin;
+  		if (current > 1000){
+    		clearInterval(id);
+    		current = 1000; // 1000以上になっているので，調整する
+  		}	
+  	w.style.top = top - current / 10 + 'px';
+  	w.style.left = left - current / 10 + 'px';
+	}, 10); // 10ms置きに実行
+	
+	//羽
+	var b = document.getElementById('b');
+	
+	var id = setInterval(function(){	//繰り返しタイマーイベント
+		var current = new Date() - begin;
+  		if (current > 1000){
+    		clearInterval(id);
+    		current = 1000; // 1000以上になっているので，調整する
+  		}	
+  	b.style.top = top - current / 10 + 'px';
+  	b.style.left = left - current / 10 + 'px';
+	}, 10); // 10ms置きに実行
+
+});
