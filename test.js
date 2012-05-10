08d6c45c43d7075f8ce5b3809eca71a6bed4489c
arisin <monokesi4120@gmail.com>
2012-05-10 15:19:29 +0900

    5/10

--- test.js
+++ test.js
@@ -7,33 +7,45 @@ window.addEventListener("load", function(){
 	//初期設定の値
 	var top = 300;
 	var left = 500;
+	var cnt = 1;
 	
-	//体
 	var begin = new Date().getTime();//時間を取得（ミリ秒）
 
+	//体
 	var	w = document.getElementById('w');
-
-	var id = setInterval(function(){	//繰り返しタイマーイベント
-		var current = new Date() - begin;
-  		if (current > 1000){
-    		clearInterval(id);
-    		current = 1000; // 1000以上になっているので，調整する
-  		}	
-  	w.style.top = top - current / 10 + 'px';
-  	w.style.left = left - current / 10 + 'px';
-	}, 10); // 10ms置きに実行
 	
 	//羽
 	var b = document.getElementById('b');
 	
-	var id = setInterval(function(){	//繰り返しタイマーイベント
+	//配列
+	var ob = [w,b];
+	
+// 10ms置きに実行
+
+	var id = setInterval(function(){
 		var current = new Date() - begin;
   		if (current > 1000){
     		clearInterval(id);
     		current = 1000; // 1000以上になっているので，調整する
-  		}	
-  	b.style.top = top - current / 10 + 'px';
-  	b.style.left = left - current / 10 + 'px';
-	}, 10); // 10ms置きに実行
+  		}
+//  		w.style.top = top - current / 10 + 'px';
+//  		w.style.left = left - current / 10 + 'px';
+//			b.style.top = top - current / 10 + 'px';
+//  		b.style.left = left - current / 10 + 'px';
+		
+		for(var i = 0; i < ab.length; i++){
+			ob[i].style.top = top - current / 10 + 'px';
+			ob[i].style.left = left - current / 10 + 'px';
+		}
 
+		cnt++;
+		if(cnt == 10){
+			stopid();
+		}
+		console.log('cnt : ' + cnt);
+	},10);
+	
+	function stopid(){
+		clearInterval(id)
+	}
 });
